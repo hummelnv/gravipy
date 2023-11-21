49de5a51c96f8415f41e680f231140960cf7cf55
hummelnv <88358679+hummelnv@users.noreply.github.com>
2023-11-21 15:45:56 -0500

    Update elevation.py

--- gravipy/elevation.py
+++ gravipy/elevation.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def latitude_correction(g_measured: np.ndarray, lat: np.ndarray) -> np.ndarray:
     return np.zeros_like(g_measured)
-
+#orthometric elevation of the Green Building is 42.23 m above the geoid according to https://observablehq.com/@earthscope/geoid-height-calculator/2#code-origin
 
 def free_air_correction(g_measured: np.ndarray) -> np.ndarray:
     return np.zeros_like(g_measured)
